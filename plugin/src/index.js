b67a0d4040889408d8344961ad80e45810855103
Manuel Pretelin <m_pretelin@hotmail.com>
2019-09-09 14:16:05 -0500

    chore(ui): add intro UI

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,22 +1,116 @@
 // @flow
 import * as React from "react";
-import { View, Text } from "react-native";
-
-const styles = {
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "green"
-  }
-};
+import { View, Text, Dimensions, Image } from "react-native";
+const { width, height } = Dimensions.get('window');
 
 export class OCLoginPluginComponent extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{color: "red"}}>PLUGIN</Text>
+        <View style={styles.subContainer}>
+          <Text style={styles.title}>Create an account to personalize your Olympic Channel experience</Text>
+          <View style={styles.imagesContainer}>
+            <View style={{ ...styles.imgInfo, alignItems: 'center' }}>
+              <Image
+                style={{ width: 120, height: 120 }}
+                resizeMode="contain"
+                source={{ uri: 'https://assets-production.applicaster.com/static/olympic-channel/images/newsletter.png' }}
+              />
+              <Text style={styles.imgTitle}>Newsletter</Text>
+            </View>
+            <View style={{ ...styles.imgInfo, alignItems: 'center' }}>
+              <Image
+                style={{ width: 120, height: 120 }}
+                resizeMode="contain"
+                source={{ uri: 'https://assets-production.applicaster.com/static/olympic-channel/images/favorites.png' }}
+              />
+              <Text style={styles.imgTitle}>Favorites</Text>
+            </View>
+            <View style={{ ...styles.imgInfo, alignItems: 'center' }}>
+              <Image
+                style={{ width: 120, height: 120 }}
+                resizeMode="contain"
+                source={{ uri: 'https://assets-production.applicaster.com/static/olympic-channel/images/play.png' }}
+              />
+              <Text style={styles.imgTitle}>Continue Watching</Text>
+            </View>
+            <View style={{ ...styles.imgInfo, alignItems: 'center' }}>
+              <Image
+                style={{ width: 120, height: 120 }}
+                resizeMode="contain"
+                source={{ uri: 'https://assets-production.applicaster.com/static/olympic-channel/images/devices.png' }}
+              />
+              <Text style={styles.imgTitle}>Sync-Accross Devices</Text>
+            </View>
+          </View>
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Text style={styles.buttonTitle}>Sign In / Register</Text>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  container: {
+    width,
+    height,
+    backgroundColor: "#E2E2E2",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  subContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -100
+  },
+  imagesContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  imgInfo: {
+    justifyContent: 'center',
+  },
+  imgTitle: {
+    fontFamily: "sans-serif",
+    fontSize: '24px',
+    fontWeight: 'bold',
+    color: "#525A5C",
+    marginTop: 50,
+  },
+  title: {
+    color: "#525A5C",
+    fontFamily: "sans-serif",
+    fontSize: '36px',
+    fontWeight: 'bold',
+    marginBottom: 110
+  },
+  buttonContainer: {
+    width: '100%',
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    width: 500,
+    height: 80,
+    backgroundColor: "#D8D8D8",
+    borderWidth: 1,
+    borderColor: '#979797',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100
+  },
+  buttonTitle: {
+    color: "#525A5C",
+    fontFamily: "sans-serif",
+    fontSize: '24px',
+    fontWeight: 'bold'
+  }
+};

3327b578197a2183d675319697e9683c70f83691
Manuel Pretelin <m_pretelin@hotmail.com>
2019-09-12 13:52:37 -0500

    chore(qr): increase size

--- plugin/src/components/QRCode.js
+++ plugin/src/components/QRCode.js
@@ -4,7 +4,7 @@ import { Image } from "react-native";
 export default function QRButton({ url }) {
   return (
     <Image
-      style={{ width: 300, height: 300 }}
+      style={{ width: 330, height: 330 }}
       resizeMode="contain"
       source={{ uri: `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=L|1&chl=${url}` }}
     />

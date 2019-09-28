e540eaebec7571464703eb832f0ccc738f2820ad
Manuel Pretelin <m_pretelin@hotmail.com>
2019-09-28 05:39:40 -0500

    chore(layaout): new design

--- plugin/src/components/Layout.js
+++ plugin/src/components/Layout.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Dimensions, View, ImageBackground, Image } from "react-native";
-import BGImage from "../../static/oc-bg.png"
-import OCLogo from "../../static/oc-logo.png"
 
-const { width, height } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 
 class Layout extends Component {
   render() {
     return (
-      <ImageBackground source={{ uri: "https://assets-production.applicaster.com/static/olympic-channel/images/oc-bg.png"}} style={styles.container}>
+      <View style={styles.container}>
         <View style={styles.logoContainer}>
           <Image
             style={styles.logo}
@@ -19,34 +17,35 @@ class Layout extends Component {
         <View style={styles.subContainer}>
           {this.props.children}
         </View>
-      </ImageBackground>
+      </View>
     );
   }
 }
 
 const styles = {
   container: {
-    width,
+    flex: 1,
     height,
-    backgroundColor: "#E2E2E2",
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    paddingTop: 240
+    backgroundColor: "#F1F1F1",
+    alignItems: 'center',
+  },
+  logoContainer: {
+    width: 150,
+    height: 150,
+    paddingLeft: 100,
+    paddingTop: 50,
+    alignSelf: 'flex-start',
   },
   subContainer: {
     flex: 1,
-    alignItems: 'flex-start',
+    width: 1390,
+    alignItems: 'center',
     justifyContent: 'center',
-  },
-  logoContainer: {
-    paddingLeft: 120,
-    paddingTop: 80,
-    paddingRight: 120
+    paddingTop: 100,
   },
   logo: {
-    width: 353,
-    height: 353,
+    width: 150,
+    height: 150,
   }
 };
 

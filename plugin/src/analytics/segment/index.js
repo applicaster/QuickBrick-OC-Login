144425eb25855dd9ad5b284eca3a2fcdf729ab8c
Manuel Pretelin <m_pretelin@hotmail.com>
2019-10-24 15:12:01 -0500

    chore(analytic): add anonymousId

--- plugin/src/analytics/segment/index.js
+++ plugin/src/analytics/segment/index.js
@@ -5,9 +5,17 @@ import { getAppData } from "@applicaster/zapp-react-native-bridge/QuickBrick";
 const TRACK_URL = "https://api.segment.io/v1/track"
 const IDENTIFY_URL = "https://api.segment.io/v1/identify"
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 export function trackEvent(screen, payload = {}, previousPage = "") {
   axios.post(TRACK_URL,
     {
+      "anonymousId": uuidv4(),
       "event": `Gygia - ${screen}`,
       "properties": {
         "deviceType": Platform.OS,

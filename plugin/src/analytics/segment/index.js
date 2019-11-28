b8ef83eeed0f31955a7bc680275c65e28e11fb76
Manuel Pretelin <m_pretelin@hotmail.com>
2019-11-28 15:17:59 -0600

    chore(analytics): update convetions

--- plugin/src/analytics/segment/index.js
+++ plugin/src/analytics/segment/index.js
@@ -22,7 +22,7 @@ export function trackEvent(segmentKey, screen, payload = {}, previousPage = "") 
   axios.post(TRACK_URL,
     {
       ...userId,
-      "event": `Gygia - ${screen}`,
+      "name": `Gygia ${screen}`,
       "properties": {
         "deviceType": Platform.OS,
         "previousPage": previousPage,
